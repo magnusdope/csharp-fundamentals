87b20a2b800dae1aaf054ec5d7250a63ef89a3c5
call me hacker man <magnuserleben@hotmail.com>
2022-02-11 23:45:31 +0100

    Create ForInteraction.cs

---
+++ ForInteraction.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace ForInteration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            for (int i = 0; i < 15; i++)
+            {
+                //Console.WriteLine(i); Udskriver 1 til 14
+
+                if (i == 6)
+                {
+                    Console.WriteLine("You found 6");
+                }
+            }
+        }
+    }
+}
