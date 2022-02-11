9d282cea1feecb360d13d2d53d3d6fd07df1515d
call me hacker man <magnuserleben@hotmail.com>
2022-02-11 20:24:43 +0100

    Create operators.cs

---
+++ operators.cs
@@ -0,0 +1,71 @@
+using System;
+
+namespace OperatorsExpressionsStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variable declaration
+            int x, y, a, b;
+
+            // Assigment operator
+            x = 3;
+            y = 2;
+            a = 1;
+            b = 0;
+
+            // Addition operator
+            x = 4 + 3;
+
+            // Substraction operator
+            x = 4 - 3;
+
+            // Multiplication operator
+            x = 10 * 5;
+
+            // Division operator
+            x = 10 / 5;
+
+            // Order of operations using parenthesis
+            x = (x + y) * (a - b);
+
+
+
+            // Equality operator
+            if (x == y)
+            {
+            }
+
+            // Greater than operator
+            if (x > y)
+            {
+            }
+
+            // Less than operator
+            if (x < y)
+            {
+            }
+
+            // Greater or equal to operator 
+            if (x >= y)
+            {
+            }
+
+            // Less or equal to operator 
+            if (x <= y)
+            {
+            }
+
+            // Conditional AND operator ...
+            if((x > y) && (a > b)) 
+            { 
+            }
+
+            // Conditional OR operator ... 
+            if ((x > y) || (a > b))
+            {
+            }
+        }
+    }
+}
