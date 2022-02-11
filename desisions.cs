4516bdc862a0f7d75aa683839aeb3f6226aecd8f
call me hacker man <magnuserleben@hotmail.com>
2022-02-11 20:26:02 +0100

    Create desisions.cs

---
+++ desisions.cs
@@ -0,0 +1,48 @@
+using System;
+
+namespace Desisions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Console.WriteLine("Bob's Big Giveaway");
+            Console.Write("Choose a door: 1, 2 or 3: ");
+            string userValue = Console.ReadLine();
+            
+            string message = "";
+
+            if (userValue == "1")
+                message = "You won a new car!";
+            else if (userValue == "2")
+                message = "You won a boat!";
+            else if (userValue == "3")
+                message = "You won a cat!";
+            else { 
+                message = "Sorry, we didn't understand. ";
+                message += "You Lose.";
+            }
+
+            Console.WriteLine(message);
+            Console.ReadLine();
+            */
+
+
+            Console.WriteLine("Bob's Big Giveaway");
+            Console.Write("Choose a door: 1, 2 or 3: ");
+            string userValue = Console.ReadLine();
+            string message = (userValue == "1") ? "boat" : "strand of lint";
+
+            //Console.Write("You won a ");
+            //Console.Write(message);
+            //Console.Write(".");
+
+            // {0} er det første index som er uservalue,
+            // {1} er message, en nemmere måde at udskrive værdigerne på som useren har indskrevet.
+            Console.WriteLine("You entered: {0}, therefore you won a {1}", userValue, message);
+            Console.ReadLine();
+
+        }
+    }
+}
