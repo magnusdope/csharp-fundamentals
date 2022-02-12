f630273f55535e00a7e56a5b111fd469637dcd14
call me hacker man <magnuserleben@hotmail.com>
2022-02-12 22:35:04 +0100

    Create simpelmethod.cs

---
+++ simpelmethod.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace SimpelMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloWorld();
+            Console.ReadLine();
+        }
+
+        private static void HelloWorld()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
